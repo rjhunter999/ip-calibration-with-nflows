93e77c335c83829c8853e4a99fcf550d5c406c75
Ross Hunter <ross.john.hunter@cern.ch>
2024-07-24 15:54:10 +0100

    Put together the transforms and make a plot to benchmark that

--- scripts/ip_correction_flow.py
+++ scripts/ip_correction_flow.py
@@ -11,22 +11,20 @@ def plot_path(sample_type, sfx):
     return f"plots/spline_flow__{sample_type}__{sfx}.png"
 
 
-def plot_single_transform(target_sample, transform):
+def plot_single_transform(target_sample: torch.Tensor, init_sample: torch.Tensor, transform: transforms.Transform, plot_path: str, nbins=60, xrange=(-4, 2)):
 
-    bin_opts = dict(bins=60, range=(-4, 2))
-    target_data = utils.load_data(target_sample, 1000)
-    target_hist, bins = np.histogram(target_data, **bin_opts)
+    bin_opts = dict(bins=nbins, range=xrange)
+    target_hist, bins = np.histogram(target_sample, **bin_opts)
 
-    noise = torch.randn(1000, 1)
-    transformed = transforms.InverseTransform(transform)(noise)
+    transformed = transforms.InverseTransform(transform)(init_sample)
 
     _, ax = plt.subplots()
     bin_centres = (bins[1:] + bins[:-1]) / 2
-    ax.errorbar(bin_centres, target_hist, yerr=np.sqrt(target_hist), fmt='+', label=f"{target_sample} target", color='black')
-    ax.hist(noise.numpy(), bins, histtype="stepfilled", label='Init. noise', facecolor='blue', alpha=0.3)
+    ax.errorbar(bin_centres, target_hist, yerr=np.sqrt(target_hist), fmt='+', label="Target", color='black')
+    ax.hist(init_sample.numpy(), bins, histtype="stepfilled", label='Initial', facecolor='blue', alpha=0.3)
     ax.hist(transformed[0].numpy(), bins, histtype="stepfilled", label='Transformed', facecolor='red', alpha=0.3)
     ax.legend()
-    plt.savefig(plot_path(target_sample, "transform_validation"))
+    plt.savefig(plot_path)
 
 
 def main():
@@ -35,16 +33,18 @@ def main():
         os.makedirs("plots")
 
     n_candidates = 1000
-    torch.manual_seed(42)
+    datasets = {target_sample: utils.load_data(target_sample, n_candidates) for target_sample in ('Z', 'DATA')}
+
     # Train two flows, one to model the LHCb data, and one to model the LHCb simulation
-    transforms = {}
+    torch.manual_seed(42)
+    trained_transforms = {}
     for target_sample in ('Z', 'DATA'):
         print(f"INFO:\tTraining flow for {target_sample}...")
         print('-'*80)
 
         print("INFO:\tLoading target data...")
         print(f"INFO:\tWill use {n_candidates} candidates throughout...")
-        target_data = utils.load_data(target_sample, n_candidates)
+        target_data = datasets[target_sample]
         utils.plot_from_sample(target_data, plot_path(target_sample, "target_sample"))
 
         print("INFO:\tInstantiating spline flow...")
@@ -62,13 +62,22 @@ def main():
         with torch.inference_mode():
             utils.benchmark_hep_style(quad_flow, target_data, plot_path=plot_path(target_sample, "performance_plots"))
 
-        transforms[target_sample] = transform
-        print(f"INFO:\tFlow training complete for target sample '{target_sample}'. Please find plots under plots/")
+        trained_transforms[target_sample] = transform
+        print(f"INFO:\tFlow training complete for target sample '{target_sample}'")
 
     # Make sure each transform individually is working
-    for target_sample, transform in transforms.items():
+    for target_sample, transform in trained_transforms.items():
         with torch.inference_mode():
-            plot_single_transform(target_sample, transform)
+            init_noise = torch.randn(1000, 1)
+            plot_single_transform(datasets[target_sample], init_noise, transform, plot_path(target_sample, "transform_validation"))
+
+    print("INFO:\tPutting the two transforms together...")
+    # A forward transform goes from target -> noise
+    sim_to_data_transform = transforms.CompositeTransform(
+        [trained_transforms["Z"], transforms.InverseTransform(trained_transforms["DATA"])]
+    )
+    with torch.inference_mode():
+        plot_single_transform(target_sample=datasets['DATA'], init_sample=datasets['Z'], transform=sim_to_data_transform, plot_path=plot_path("Z_to_DATA", "transform_validation"), nbins=100, xrange=(-4, 0))
 
 
 if __name__ == "__main__":
