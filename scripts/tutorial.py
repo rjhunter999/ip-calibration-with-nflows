7aa44ef34f41d495b6a5f43dcea3b61285489dac
Ross Hunter <ross.john.hunter@cern.ch>
2024-08-18 16:53:47 +0100

    Run pre-commit ; cleanup formatting

--- scripts/tutorial.py
+++ scripts/tutorial.py
@@ -3,15 +3,21 @@ import torch
 import matplotlib.pyplot as plt
 import os
 
+
 def make_basic_flow(hidden_features=4):
     # Need to learn what these transforms do/why we might want to use these.
-    transform = transforms.CompositeTransform([
-        transforms.MaskedAffineAutoregressiveTransform(features=2, hidden_features=hidden_features),
-        transforms.RandomPermutation(features=2)
-    ])
+    transform = transforms.CompositeTransform(
+        [
+            transforms.MaskedAffineAutoregressiveTransform(
+                features=2, hidden_features=hidden_features
+            ),
+            transforms.RandomPermutation(features=2),
+        ]
+    )
     base_distribution = distributions.StandardNormal(shape=[2])
     flow = flows.Flow(transform=transform, distribution=base_distribution)
     return flow
+
 
 def plot_from_sample(samples, plot_sfx):
     plt.figure()
@@ -25,11 +31,12 @@ def sample_flow(flow, n_samples):
         print(f"Taken {n_samples} samples from flow.\nsamples.shape: {samples.shape}")
         return samples
 
+
 def train_flow(flow, target):
     optimizer = torch.optim.Adam(flow.parameters())
     n_iter = 5000
     for i in range(n_iter):
-        #flow.train() # Put it in training mode.
+        # flow.train() # Put it in training mode.
 
         # TODO Need to understand why we zero_grad() before calculating the loss.
         # TODO also need to understand the loss
@@ -52,8 +59,9 @@ def train_flow(flow, target):
                 zgrid = flow.log_prob(xyinput).exp().reshape(100, 100)
 
                 plt.contourf(xgrid.numpy(), ygrid.numpy(), zgrid.numpy())
-                plt.title('iteration {}'.format(i + 1))
+                plt.title("iteration {}".format(i + 1))
                 plt.savefig(f"plots/tutorial__flow_training__iteration{i}.png")
+
 
 def main():
     if not os.path.exists("plots"):
@@ -76,6 +84,7 @@ def main():
     train_flow(flow, target)
     plot_from_sample(sample_flow(flow, n_samples), "flow_sample_posttrain")
     print("INFO:\tFlow training complete. Please find plots under plots/")
+
 
 if __name__ == "__main__":
     main()
